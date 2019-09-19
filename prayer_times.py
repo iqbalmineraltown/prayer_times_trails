8f9a9d15698875575d0c82841b0d46022436658c
Iqbal <iqbalmineraltown@gmail.com>
2019-09-19 21:25:42 +0700

    add starting message

--- prayer_times.py
+++ prayer_times.py
@@ -9,7 +9,9 @@ jsonurl = f'http://muslimsalat.com/{cityname}.json'
 username = 'Haji Oval'
 emojicon = ':kaaba:'
 webhookurl = 'https://hooks.slack.com/services/<secret>'
+
 advminutes = 10
+start_message = f'Hai, {username} akan membantu mengingatkan jadwal sholat sekitar {advminutes} menit sebelum waktu adzan berkumandang.\nBerikut jadwal sholat untuk {cityname} hari ini:\n'
 
 
 def slack_sender(name: str, time: datetime):
@@ -33,11 +35,7 @@ def slack_sender(name: str, time: datetime):
     pass
 
 
-def reminder_prayertime(name: str, time: str):
-    time_sched = substract_10_mins(time)
-    today = datetime.today()
-    today_date = datetime(today.year, today.month, today.day,
-                          time_sched.hour, time_sched.minute)
+def convert_prayer_name(name: str) -> str:
 
     if name == 'fajr':
         name = 'Subuh'
@@ -50,6 +48,21 @@ def reminder_prayertime(name: str, time: str):
     else:
         name = name.title()
 
+    return name
+    pass
+
+
+def reminder_prayertime(name: str, time: str):
+    time_sched = substract_10_mins(time)
+    today = datetime.today()
+    today_date = datetime(today.year, today.month, today.day,
+                          time_sched.hour, time_sched.minute)
+
+    name = convert_prayer_name(name)
+    time_prayer = time_sched.strftime('%H:%M')
+    global start_message
+    start_message += f'{name} {time_prayer}\n'
+
     slack_sender(name, today_date)
     pass
 
@@ -58,9 +71,21 @@ def fetch_prayertimes():
     os.system('for i in `atq | awk \'{print $1}\'`;do atrm $i;done')
 
     resjson = json.loads(requests.get(jsonurl).text)
+
     for key, val in resjson['items'][0].items():
         if not (key == 'date_for' or key == 'shurooq'):
             reminder_prayertime(key, val)
+
+    global start_message
+    payloaddict = {
+        'username': username,
+        'text': start_message,
+        'icon_emoji': emojicon
+    }
+    json_dump = json.dumps(payloaddict)
+
+    os.system(f'curl -X POST {webhookurl} -d \'{json_dump}\'')
+
     pass
 
 
